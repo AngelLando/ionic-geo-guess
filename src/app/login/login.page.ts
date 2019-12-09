4e67cff419444ca363bc57d827f152b0be311c40
Angel Lando <angel.lando@outlook.com>
2019-12-09 18:05:44 +0100

    Implemented authentication service

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
+import { AuthService } from '../auth/auth.service';
+import { AuthRequest } from '../models/auth-request';
+
+/**
+ * Login page.
+ */
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html'
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  /**
+   * This authentication request object will be updated when the user
+   * edits the login form. It will then be sent to the API.
+   */
+  authRequest: AuthRequest;
 
-  ngOnInit() {
+  /**
+   * If true, it means that the authentication API has return a failed response
+   * (probably because the name or password is incorrect).
+   */
+  loginError: boolean;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.authRequest = new AuthRequest();
   }
 
+  /**
+   * Called when the login form is submitted.
+   */
+  onSubmit(form: NgForm) {
+
+    // Do not do anything if the form is invalid.
+    if (form.invalid) {
+      return;
+    }
+
+    // Hide any previous login error.
+    this.loginError = false;
+
+    // Perform the authentication request to the API.
+    this.auth.logIn(this.authRequest).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/home');
+      },
+      error: err => {
+        this.loginError = true;
+        console.warn(`Authentication failed: ${err.message}`);
+      }
+    });
+  }
 }
