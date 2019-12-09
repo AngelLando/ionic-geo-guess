9186e716313c980b3bc32a46a4fa15ef20fe2ab7
Angel Lando <angel.lando@outlook.com>
2019-12-09 16:08:21 +0100

    Added tabs

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,27 +6,29 @@ import { HomePage } from './home.page';
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
-  },
-  {
-    path: 'scores',
-    loadChildren: () => import('./scores/scores.module').then( m => m.ScoresPageModule)
-  },
-  {
-    path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
-  },
-  {
-    path: 'create-thumbnail',
-    loadChildren: () => import('./create-thumbnail/create-thumbnail.module').then( m => m.CreateThumbnailPageModule)
-  },
-  {
-    path: 'my-thumbnails',
-    loadChildren: () => import('./my-thumbnails/my-thumbnails.module').then( m => m.MyThumbnailsPageModule)
-  },
-  {
-    path: 'all-thumbnails',
-    loadChildren: () => import('./all-thumbnails/all-thumbnails.module').then( m => m.AllThumbnailsPageModule)
+    component: HomePage,
+    children: [
+      {
+        path: 'scores',
+        loadChildren: () => import('./scores/scores.module').then( m => m.ScoresPageModule)
+      },
+      {
+        path: 'settings',
+        loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+      },
+      {
+        path: 'create-thumbnail',
+        loadChildren: () => import('./create-thumbnail/create-thumbnail.module').then( m => m.CreateThumbnailPageModule)
+      },
+      {
+        path: 'my-thumbnails',
+        loadChildren: () => import('./my-thumbnails/my-thumbnails.module').then( m => m.MyThumbnailsPageModule)
+      },
+      {
+        path: 'all-thumbnails',
+        loadChildren: () => import('./all-thumbnails/all-thumbnails.module').then( m => m.AllThumbnailsPageModule)
+      }
+    ]
   }
 ];
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface HomePageTab {
+  title: string;
+  icon: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
+  tabs: HomePageTab[];
 
-  ngOnInit() {
+  constructor() {
+    this.tabs = [
+      { title: 'Home', icon: 'home', path: 'all-thumbnails' },
+      { title: 'Scores', icon: 'trophy', path: 'scores'},
+      { title: 'My thumbnails', icon: 'list', path: 'my-thumbnails'},
+      { title: 'Settings', icon: 'settings', path: 'settings'},
+    ];
   }
 
 }
