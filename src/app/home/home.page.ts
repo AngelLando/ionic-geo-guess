754a61b1e9519f1a6b031f583eb0d9093205a6e8
Angel Lando <angel.lando@outlook.com>
2019-12-19 11:36:40 +0100

    Fixed tab bar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,7 @@ export class HomePage {
     this.tabs = [
       { title: 'Home', icon: 'home', path: 'all-thumbnails' },
       { title: 'Scores', icon: 'trophy', path: 'scores'},
-      { title: 'test add', icon: 'list', path: 'create-thumbnail'},
+      { title: '', icon: '', path: ''},
       { title: 'My thumbnails', icon: 'list', path: 'my-thumbnails'},
       { title: 'Settings', icon: 'settings', path: 'settings'},
     ];
