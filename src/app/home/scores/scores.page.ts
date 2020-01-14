b5e3b5e011241d067a3dd6917e10f71194c91e07
Pauline Baeni <pauline.baeni@heig-vd.ch>
2020-01-14 17:14:02 +0100

    implemented filters

--- src/app/home/scores/scores.page.ts
+++ src/app/home/scores/scores.page.ts
@@ -13,8 +13,12 @@ import { User } from 'src/app/models/user';
 export class ScoresPage implements OnInit {
 
   users: User[];
+  filteredUsers: User[];
   location: string;
+  input:string;
   contentLoaded = false;
+  countryList=[];
+  cityList=[];
 
   constructor(private http: HttpClient) {
     this.users = [];
@@ -28,11 +32,50 @@ export class ScoresPage implements OnInit {
       this.contentLoaded = true;
       // Order by totalScore
       this.users.sort((a, b) => b.totalScore - a.totalScore);
+      this.getAllCountries();
+      this.getAllCities();
+      this.filteredUsers=this.users;
     });
   }
 
+  getAllCountries(){
+    const tab = [];
+    this.users.forEach(function(item){
+      if (item.country!=null){
+tab.push(item.country);
+      }
+    });
+  const uniqueTab = tab.filter((x,i,a)=> a.indexOf(x)==i);
+    this.countryList = uniqueTab;
+  }
+
+  
+  getAllCities(){
+    const tab = [];
+    this.users.forEach(function(item){
+      if (item.city!=null){
+tab.push(item.city);
+      }
+    });
+  const uniqueTab = tab.filter((x,i,a)=> a.indexOf(x)==i);
+    this.cityList = uniqueTab;
+  }
+
   segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
+    this.input==null;
+    this.filteredUsers=this.users;
+  }
+
+  showSelectedValue(ev:any){
+console.log(this.input)
+  }
+
+  filterUser(ev:any){
+    if(this.location=='country'){
+      this.filteredUsers= this.users.filter(user=> user.country==this.input);
+    }else{
+      this.filteredUsers= this.users.filter(user=> user.city==this.input);
+    }
   }
 
   doRefresh(ev: any) {
